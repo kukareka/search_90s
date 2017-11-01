8a2ea6b2b83a8219df0a8e0f705c8780b328a6f4
Oleg Kukareka <oleg@kukareka.com>
2017-11-01 19:25:00 +0200

    Add filter by type

--- app/queries/document_query.rb
+++ app/queries/document_query.rb
@@ -1,6 +1,12 @@
-class DocumentQuery < Struct.new(:query)
+class DocumentQuery < Struct.new(:query, :types)
   def results
-    Document.with_terms(query_terms)
+    scope = Document.with_terms(query_terms)
+    scope = scope.where(type: document_types) if types.present?
+    scope
+  end
+
+  def document_types
+    types.map(&:singularize).map(&:camelize)
   end
 
   def query_terms
